1325480b4395f2f560ded389c92a304e66bffe0f
Feleys <30944938+Feleys@users.noreply.github.com>
2018-03-08 13:40:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
+// 'show_in_menu' => 'edit.php?post_type=page_article'
 
 
-
-
-add_action( 'admin_menu', 'my_plugin_menu' );
  function my_plugin_menu(){
    add_menu_page('Page title', 'Top-level menu title', 'manage_options',  'my-top-level-handle', 'my_menu_function');
    add_submenu_page( 'my-top-level-handle', 'Custom Post Type Admin', 'Articles', 'manage_options','edit.php?post_type=page_article');
- }
+ 
+ 
+ add_action( 'admin_menu', 'my_plugin_menu' );}
