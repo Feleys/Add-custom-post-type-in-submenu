cca6419e7e2a5d964043bf0389f3c0eaa1c33fb3
Feleys <30944938+Feleys@users.noreply.github.com>
2018-03-08 13:40:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+
+
+add_action( 'admin_menu', 'my_plugin_menu' );
+ function my_plugin_menu(){
+   add_menu_page('Page title', 'Top-level menu title', 'manage_options',  'my-top-level-handle', 'my_menu_function');
+   add_submenu_page( 'my-top-level-handle', 'Custom Post Type Admin', 'Articles', 'manage_options','edit.php?post_type=page_article');
+ }
